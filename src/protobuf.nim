3222b1d69ca77fe43652bd1757593ed57f2c66db
Ivan Koster <ivankoster@gmail.com>
2017-03-20 00:45:19 +0100

    Add PbMessage macro to generate message types,
    including their OneOf construct types and all their assignment templates and access procs
    
    A little bit of cleanup is still needed because WriteTo and MergeFrom are broken because of these additions

--- src/protobuf.nim
+++ src/protobuf.nim
@@ -1,5 +1,8 @@
 import macros
 
+static:
+  var pbMessageFields: NimNode
+
 type
   CodedOutputStream* = seq[byte]
   WireType* = enum
@@ -112,20 +115,6 @@ proc SkipField*(this: CodedInputStream, tag: uint) =
   of EndGroup: assert(false) # Not implemented yet
   of Fixed32: assert(false) # Not implemented yet
 
-macro PbWriteTo*(obj: typed, output: CodedOutputStream, fieldInfo: untyped): untyped =
-  expectKind(fieldInfo, nnkStmtList)
-  result = newStmtList()
-
-  for field in fieldInfo:
-    expectKind(field, nnkCall)
-    var fieldIdentifier = field[0]
-    expectKind(field[1][0], nnkCommand)
-    expectKind(field[1][0][1], nnkPrefix)
-    var fieldNumberLiteral = field[1][0][1][1]
-    result.add(newIfStmt((infix(newDotExpr(ident($obj.symbol), fieldIdentifier), "!=", newIntLitNode(0)),
-                          newStmtList(newCall("WriteTag", ident($output[0].symbol), fieldNumberLiteral, ident("Varint")),
-                                      newCall("WriteInt32", ident($output[0].symbol), newDotExpr(ident($obj.symbol), fieldIdentifier))))))
-
 macro PbMergeFrom*(obj: typed, input: CodedInputStream, fieldInfo: untyped): untyped =
   expectKind(fieldInfo, nnkStmtList)
 
@@ -156,3 +145,230 @@ macro PbMergeFrom*(obj: typed, input: CodedInputStream, fieldInfo: untyped): unt
     var `tagIdentifier`: uint
     while (`tagIdentifier` = input.ReadTag(); `tagIdentifier` != 0):
       `caseBody`
+
+
+
+
+
+# -----------------------------------------------
+#TODO put in seperate file
+
+
+
+
+proc pbFieldTypeIdentToNimTypeIdent(fieldType: NimNode): NimNode =
+  case $fieldType
+  of "PbBytes": result = parseExpr("seq[byte]")
+  of "PbString": result = newIdentNode("string")
+  else:
+    error("Cannot handle given fieldType " & $fieldType, fieldType)
+
+proc callToFieldInfo(call: NimNode): (NimNode, NimNode, NimNode) =
+  expectKind(call, nnkCall)
+  let fieldName = call[0]
+  expectKind(call[1], nnkStmtList)
+  expectKind(call[1][0], nnkCommand)
+  let fieldType = call[1][0][0]
+  expectKind(call[1][0][1], nnkPrefix)
+  let fieldNumber = call[1][0][1][1]
+  (fieldName, fieldType, fieldNumber)
+
+proc commandToOneOfInfo(command: NimNode): (NimNode, NimNode) =
+  expectKind(command, nnkCommand)
+  expectKind(command[0], nnkIdent)
+  if $(command[0]) != "PbOneOf": error("Expected PbOneOf", command[0])
+  expectKind(command[1], nnkIdent)
+  let oneOfName = command[1]
+  expectKind(command[2], nnkStmtList)
+  let oneOfFields = command[2]
+  (oneOfName, oneOfFields)
+
+proc createOneOfEnum(oneOfKindName, oneOfFields: NimNode): NimNode =
+  var oneOfKinds: seq[NimNode] = @[newIdentNode("unspecified")]
+  for call in oneOfFields:
+    let (fieldName, _, _) = callToFieldInfo(call)
+    oneOfKinds.add(fieldName)
+
+  result = newTree(nnkTypeSection,
+    newTree(nnkTypeDef,
+      newTree(nnkPragmaExpr,
+        oneOfKindName,
+        newTree(nnkPragma, newIdentNode("pure"))),
+      newEmptyNode(),
+      newTree(nnkEnumTy,
+        newEmptyNode()
+        ).add(oneOfKinds)))
+
+proc createOneOfObjectType(oneOfFullName, oneOfKindName, oneOfFields: NimNode): NimNode =
+  var OfBranches: seq[NimNode] = @[]
+  OfBranches.add(
+    newTree(nnkOfBranch,
+      newDotExpr(
+        oneOfKindName,
+        newIdentNode("unspecified")),
+      newTree(nnkRecList,
+        newNilLit())))
+
+  for call in oneOfFields:
+    let (fieldName, fieldType, _) = callToFieldInfo(call)
+    OfBranches.add(
+      newTree(nnkOfBranch,
+        newDotExpr(
+          oneOfKindName,
+          fieldName),
+        newTree(nnkRecList,
+          newIdentDefs(
+            newIdentNode("inner_" & $fieldName),
+            pbFieldTypeIdentToNimTypeIdent(fieldType)))))
+
+  result = newTree(nnkTypeSection,
+    newTree(nnkTypeDef,
+      oneOfFullName,
+      newEmptyNode(),
+      newTree(nnkObjectTy,
+        newEmptyNode(),
+        newEmptyNode(),
+        newTree(nnkRecList,
+          newTree(nnkRecCase,
+            newIdentDefs(
+              newIdentNode("kind"),
+              oneOfKindName),
+            ).add(OfBranches)))))
+
+{.experimental.} # We use the experimental dot expressions overloading in the next proc
+proc createOneOfFieldAssignmentTemplate(oneOfShortName, oneOfFullName, oneOfKindName, oneOfFields: NimNode): NimNode =
+  var ElifBranches: seq[NimNode] = @[]
+
+  for call in oneOfFields:
+    let (fieldName, _, _) = callToFieldInfo(call)
+    let innerFieldName = newIdentNode("inner_" & $fieldName)
+
+    let assignmentStatements = quote do:
+      if `oneOfShortName`.kind != `oneOfKindName`.`fieldName`:
+        `oneOfShortName`.reset
+        `oneOfShortName`.kind = `oneOfKindName`.`fieldName`
+      `oneOfShortName`.`innerFieldName` = value
+
+    ElifBranches.add(
+      newTree(nnkElifBranch,
+        infix(newIdentNode("field"), "==", newStrLitNode($fieldName)),
+        assignmentStatements))
+
+  ElifBranches.add(
+    newTree(nnkElse, # non-existing field is assigned
+          parseStmt("""{.fatal: "expression '" & field & "=' cannot be called".}""" )))
+
+  result = newProc(
+    newTree(nnkAccQuoted,
+      newIdentNode(".=")),
+    [newEmptyNode(),
+      newIdentDefs(
+        oneOfShortName,
+        oneOfFullName),
+      newIdentDefs(
+        newIdentNode("field"),
+        newIdentNode("string")),
+      newIdentDefs(
+        newIdentNode("value"),
+        newIdentNode("typed"))],
+    newStmtList(
+      newTree(nnkWhenStmt,
+        ElifBranches)),
+    nnkTemplateDef)
+
+proc createOneOfFieldAccessProcs(oneOfShortName, oneOfFullName, oneOfFields: NimNode): seq[NimNode] =
+  result = @[]
+  for call in oneOfFields:
+    let (fieldName, fieldType, _) = callToFieldInfo(call)
+    result.add(newProc(
+      fieldName,
+      [pbFieldTypeIdentToNimTypeIdent(fieldType),
+        newIdentDefs(
+          oneOfShortName,
+          oneOfFullName)],
+      newStmtList(
+        newDotExpr(
+          oneOfShortName,
+          newIdentNode("inner_" & $fieldName)))))
+
+proc createOneOfTypes(stmtList, containingTypeIdent: NimNode): seq[NimNode] =
+  expectKind(stmtList, nnkStmtList)
+  result = @[]
+
+  for child in stmtList:
+    if child.kind != nnkCommand: continue
+
+    #first produce the enum containing which oneOf field is set
+    let (oneOfShortName, oneOfFields) = commandToOneOfInfo(child)
+    let oneOfFullName = newIdentNode($containingTypeIdent & "_" & $oneOfShortName)
+    let oneOfKindName = newIdentNode($containingTypeIdent & "_" & $oneOfShortName & "_kind")
+
+    result.add(createOneOfEnum(oneOfKindName, oneOfFields))
+    result.add(createOneOfObjectType(oneOfFullName, oneOfKindName, oneOfFields))
+    result.add(createOneOfFieldAssignmentTemplate(oneOfShortName, oneOfFullName, oneOfKindName, oneOfFields))
+    result.add(createOneOfFieldAccessProcs(oneOfShortName, oneOfFullName, oneOfFields))
+
+proc toMessageObjectIdentDefs(stmtList, containingTypeIdent: NimNode): seq[NimNode] =
+  expectKind(stmtList, nnkStmtList)
+  result = @[]
+
+  for child in stmtList:
+    case child.kind
+    of nnkCall:
+      let (fieldName, fieldType, _) = callToFieldInfo(child)
+      result.add(
+        newIdentDefs(
+          postfix(fieldName, "*"),
+          fieldType))
+    of nnkCommand:
+      let (oneOfShortName, _) = commandToOneOfInfo(child)
+      result.add(
+        newIdentDefs(
+          postfix(oneOfShortName, "*"),
+          newIdentNode($containingTypeIdent & "_" & $oneOfShortName)))
+    else:
+      discard
+
+proc createMessageObjectType(ident: NimNode, identDefs: seq[NimNode]): Nimnode =
+  result = newTree(nnkTypeSection,
+    newTree(nnkTypeDef,
+      postfix(ident, "*"),
+      newEmptyNode(),
+      newTree(nnkObjectTy,
+        newEmptyNode(),
+        newEmptyNode(),
+        newTree(nnkRecList, identDefs))))
+
+macro PbMessage*(typeName, fields: untyped): untyped =
+  pbMessageFields = fields
+  result = newStmtList()
+  result.add(createOneOfTypes(fields, typeName))
+  result.add(createMessageObjectType(typeName, fields.toMessageObjectIdentDefs(typeName)))
+
+macro PbWriteTo*(message: typed, output: CodedOutputStream): untyped =
+  result = newStmtList()
+
+  for child in pbMessageFields:
+    case child.kind
+    of nnkCall:
+      let (fieldName, fieldType, fieldNumber) = callToFieldInfo(child)
+      echo treeRepr(getType(fieldType))
+      var condition: NimNode
+      # if fieldType == enum: # lookup somehow if this type is an enum
+      #   condition = infix(
+      #     newCall("ord", newDotExpr(ident($message), fieldName)),
+      #     "!=", newIntLitNode(0))
+      # else:
+      #   condition = infix(
+      #     newDotExpr(ident($message), fieldName),
+      #     "!=", newIntLitNode(0))
+      # result.add(
+      #   newIfStmt(
+      #     (condition,
+      #     newStmtList(
+      #       newCall("WriteTag", ident($output[0].symbol), fieldNumber, ident("Varint")),
+      #       newCall("WriteInt32", ident($output[0].symbol), newDotExpr(ident($message), fieldName))))))
+    of nnkCommand:
+      discard
+    else:
+      discard
